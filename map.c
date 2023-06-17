9d5ebde5c4b20b91a58ad4073c5a57a22f986731
Phil-Peret <filippoperettivr@gmail.com>
2023-06-17 21:03:02 +0200

    fix printmap with value > 9

--- map.c
+++ map.c
@@ -18,7 +18,8 @@ void insert_getton_on_map(char map[], int width, int height, int pos, char symbo
 
 void print_map(char map[],int width,int height){
 	for(int i = 0; i<height; i++){
-		printf("  %i ",i);
+		if(i < 10) printf("  %i ",i);
+		else printf(" %i ",i);
 	}
 	printf("\n");
 
